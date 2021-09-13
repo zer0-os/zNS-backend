b55a4c51eeba09dc1f0a05eaaf37cdc55df48d3a
Zachary <remscar@gmail.com>
2021-09-13 12:03:55 -0700

    Added support for uploading videos to cloudinary (#13)

--- src/storage/adapters/cloudinary.ts
+++ src/storage/adapters/cloudinary.ts
@@ -1,17 +1,20 @@
 import * as cloudinary from "cloudinary";
 import * as streamifier from "streamifier";
+import { Maybe } from "../../types";
 
-export const uploadFile = async (
+const uploadStreamToCloudinary = async (
+  type: string = "image",
   publicId: string,
   data: Buffer,
   folder?: string
-): Promise<cloudinary.UploadApiResponse> => {
+) => {
   const finishedUploading = new Promise<cloudinary.UploadApiResponse>(
     (resolve, reject) => {
       const uploadStream = cloudinary.v2.uploader.upload_stream(
         {
           public_id: publicId,
           folder,
+          resource_type: type,
         },
         (err, res) => {
           if (err) {
@@ -32,7 +35,33 @@ export const uploadFile = async (
     }
   );
 
-  const response = await finishedUploading;
+  return finishedUploading;
+};
 
-  return response;
+export const uploadFile = async (
+  publicId: string,
+  data: Buffer,
+  folder?: string
+): Promise<cloudinary.UploadApiResponse> => {
+  let response: Maybe<cloudinary.UploadApiResponse>;
+
+  console.log(`Attempting to upload as video...`);
+  try {
+    response = await uploadStreamToCloudinary("video", publicId, data, folder);
+    return response;
+  } catch (e) {
+    console.debug(`Failed to upload as video.`);
+    console.debug(e);
+  }
+
+  console.log(`Attempting to upload as image...`);
+  try {
+    response = await uploadStreamToCloudinary("image", publicId, data, folder);
+    return response;
+  } catch (e) {
+    console.debug(`Failed to upload as image.`);
+    console.debug(e);
+  }
+
+  throw Error(`Unable to upload data to cloudinary as image or video.`);
 };
